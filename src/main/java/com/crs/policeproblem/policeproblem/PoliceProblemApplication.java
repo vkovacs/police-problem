5e586eafac70a11c0880aacfde70fa7528b44dc9
Viktor Kovacs <crusader8@gmail.com>
2021-07-11 15:25:00 +0200

    initial commit

---
+++ src/main/java/com/crs/policeproblem/policeproblem/PoliceProblemApplication.java
@@ -0,0 +1,13 @@
+package com.crs.policeproblem.policeproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliceProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliceProblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crs/policeproblem/policeproblem/PoliceProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crs.policeproblem.policeproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliceProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
